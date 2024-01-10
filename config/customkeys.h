08bd374db1dfac0d45e2689a32f830eff46a816d
Alexander Mück <52800213+FlawedSyntax@users.noreply.github.com>
2024-01-10 14:58:52 +0100

    Update customkeys.h

--- config/customkeys.h
+++ config/customkeys.h
@@ -1,1 +1,12 @@
+// custom keys that the standard zmk firmware doesn't provide
 
+
+#pragma once
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+
+/* less than */
+#define CL_THAN (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYPAD_LESS_THAN))
+#define CG_THAN (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYPAD_GREATER_THAN))
